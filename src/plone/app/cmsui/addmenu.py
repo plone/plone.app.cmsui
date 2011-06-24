b18fb34d656a74ee8eda2f381f8a47732fe7b6dc
Eric Steele <ems174@psu.edu>
2011-06-24 18:28:35 +0100

    Begin implementing form w/ z3c.form

--- src/plone/app/cmsui/addmenu.py
+++ src/plone/app/cmsui/addmenu.py
@@ -1,21 +1,83 @@
-from zope.app.publisher.interfaces.browser import IBrowserMenu
-from zope.component import getMultiAdapter, getUtility
-from zope.publisher.browser import BrowserView
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleTerm
+from plone.app.content.browser.folderfactories import _allowedTypes
+from plone.app.z3cform.layout import wrap_form
+from z3c.form import button, form, field
+from zope import interface, schema
+from zope.schema.interfaces import IVocabularyFactory
+from zope.component import getMultiAdapter
+from zope.interface import implements
 
 
-class AddMenu(BrowserView):
-    """Add menu overlay
-    """
+class AddableTypesVocabulary(object):
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        context = getattr(context, 'context', context)
+        request = context.REQUEST
+        
+        factories_view = getMultiAdapter((context, request), name='folder_factories')
+
+        haveMore = False
+        include = None
+
+        addContext = factories_view.add_context()
+        allowedTypes = _allowedTypes(request, addContext)
+        items = [SimpleTerm(i.id, i.id, i.Title()) for i in allowedTypes]
+        return SimpleVocabulary(items)
+
+AddableTypesVocabularyFactory = AddableTypesVocabulary()
+
+
+class IAddMenu(interface.Interface):
+
+    title = schema.TextLine(title=u"Title")
+    content_type = schema.Choice(title=u"Type", vocabulary=u"plone.app.cmsui.AddableContentTypes")
+
+
+class AddMenu(form.Form):
+    fields = field.Fields(IAddMenu)
+    ignoreContext = True # don't use context to get widget data
+    label = "Add content"
     
-    def __call__(self):
-        # Disable theming
-        self.request.response.setHeader('X-Theme-Disabled', 'True')
-        
-        factoriesMenu = getUtility(IBrowserMenu, name='plone_contentmenu_factory', context=self.context)
-        self.addable_types = factoriesMenu.getMenuItems(self.context, self.request)
+    
+    @button.buttonAndHandler(u'Add content')
+    def handleApply(self, action):
+        data, errors = self.extractData()
+        if errors:
+            return
+        # invoke factory
+        title = data['title']
+        # title-to-id
+        id = 'bob'
+        # create the object
+        self.context.invokeFactory('Document', id=id, title=title)
+        import pdb; pdb.set_trace( )
+        # redirect to immediate_view
+        # open edit overlay    
 
-        breadcrumbs_view = getMultiAdapter((self.context, self.request),
-                                           name='breadcrumbs_view')
-        self.breadcrumbs = breadcrumbs_view.breadcrumbs()
-        
-        return self.index()
+AddForm = wrap_form(AddMenu)
+
+
+
+# class AddMenu(BrowserView):
+#     """Add menu overlay
+#     """
+#     
+#     def __call__(self):
+#         # Disable theming
+#         self.request.response.setHeader('X-Theme-Disabled', 'True')
+#         
+#         factoriesMenu = getUtility(IBrowserMenu, name='plone_contentmenu_factory', context=self.context)
+#         self.addable_types = factoriesMenu.getMenuItems(self.context, self.request)
+# 
+#         breadcrumbs_view = getMultiAdapter((self.context, self.request),
+#                                            name='breadcrumbs_view')
+#         self.breadcrumbs = breadcrumbs_view.breadcrumbs()
+#         
+#         return self.index()
+# 
+# 
+#         
+#         
+#         
