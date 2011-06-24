79e8c17ed2e2378745f888681827dcf49bf48e5c
Eric Steele <ems174@psu.edu>
2011-06-24 15:01:27 +0100

    Pull in addable types.

--- src/plone/app/cmsui/addmenu.py
+++ src/plone/app/cmsui/addmenu.py
@@ -1,3 +1,5 @@
+from zope.app.publisher.interfaces.browser import IBrowserMenu
+from zope.component import getUtility
 from zope.publisher.browser import BrowserView
 
 class AddMenu(BrowserView):
@@ -7,4 +9,9 @@ class AddMenu(BrowserView):
     def __call__(self):
         # Disable theming
         self.request.response.setHeader('X-Theme-Disabled', 'True')
+        
+        factoriesMenu = getUtility(IBrowserMenu, name='plone_contentmenu_factory', context=self.context)
+        self.addable_types = factoriesMenu.getMenuItems(self.context, self.request)
+
+
         return self.index()
