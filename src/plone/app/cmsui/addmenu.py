0f579004d1d0a1d761ce6129bf286525c8635781
Eric Steele <ems174@psu.edu>
2011-06-25 00:14:05 +0100

    Render the form as a part of the view.

--- src/plone/app/cmsui/addmenu.py
+++ src/plone/app/cmsui/addmenu.py
@@ -1,3 +1,7 @@
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.component import getUtility
+from zope.app.publisher.interfaces.browser import IBrowserMenu
+from zope.publisher.browser import BrowserView
 from zope.component import queryUtility
 from plone.i18n.normalizer.interfaces import IIDNormalizer
 from plone.app.content.browser.folderfactories import _allowedTypes
@@ -8,7 +12,7 @@ from zope.component import getMultiAdapter
 from zope.interface import implements
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
+from plone.z3cform.interfaces import IWrappedForm
 
 class AddableTypesVocabulary(object):
     implements(IVocabularyFactory)
@@ -33,7 +37,9 @@ class IAddMenu(interface.Interface):
     content_type = schema.Choice(title=u"Type", vocabulary=u"plone.app.cmsui.AddableContentTypes")
 
 
-class AddMenu(form.Form):
+class AddForm(form.Form):
+    implements(IWrappedForm)
+    
     fields = field.Fields(IAddMenu)
     ignoreContext = True # don't use context to get widget data
     label = "Add content"
@@ -54,28 +60,31 @@ class AddMenu(form.Form):
         # redirect to immediate_view
         # open edit overlay    
 
-AddForm = wrap_form(AddMenu)
+# AddForm = wrap_form(AddMenu)
 
 
 
-# class AddMenu(BrowserView):
-#     """Add menu overlay
-#     """
-#     
-#     def __call__(self):
-#         # Disable theming
-#         self.request.response.setHeader('X-Theme-Disabled', 'True')
-#         
-#         factoriesMenu = getUtility(IBrowserMenu, name='plone_contentmenu_factory', context=self.context)
-#         self.addable_types = factoriesMenu.getMenuItems(self.context, self.request)
-# 
-#         breadcrumbs_view = getMultiAdapter((self.context, self.request),
-#                                            name='breadcrumbs_view')
-#         self.breadcrumbs = breadcrumbs_view.breadcrumbs()
-#         
-#         return self.index()
-# 
-# 
-#         
-#         
-#         
+class AddMenu(BrowserView):
+    """Add menu overlay
+    """
+    
+    def __call__(self):
+        # Disable theming
+        self.request.response.setHeader('X-Theme-Disabled', 'True')
+        
+        factoriesMenu = getUtility(IBrowserMenu, name='plone_contentmenu_factory', context=self.context)
+        self.addable_types = factoriesMenu.getMenuItems(self.context, self.request)
+
+        breadcrumbs_view = getMultiAdapter((self.context, self.request),
+                                           name='breadcrumbs_view')
+        self.breadcrumbs = breadcrumbs_view.breadcrumbs()
+        
+        self.form = AddForm(self.context, self.request)
+        self.form.update()
+        
+        return self.index()
+
+
+        
+        
+        
