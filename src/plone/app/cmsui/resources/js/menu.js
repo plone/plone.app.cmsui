2e1b128fbe28c1427de6022ff4009a3fc027f316
Denys Mishunov <denys.mishunov@gmail.com>
2011-06-24 13:58:40 +0200

    Fixing menu.js based on JSLint output

--- src/plone/app/cmsui/resources/js/menu.js
+++ src/plone/app/cmsui/resources/js/menu.js
@@ -1,20 +1,22 @@
+/*globals window, jQuery*/
+
 /* Code that runs inside the iframe menu
  */
 
 (function ($) {
     var cmsui = window.parent.cmsui;
     cmsui.overlay.close(function () {
-        
+
         // TODO
-        
+
     });
-    $().ready(function() {
+    $().ready(function () {
         $('a.overlayLink').click(function (e) {
             var link = $(this);
             cmsui.overlay.open(link.attr('href'), function () {
             });
             return false;
-        })
+        });
 
     });
 }(jQuery));
