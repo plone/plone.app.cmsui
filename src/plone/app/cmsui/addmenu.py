f6d5e7740f72ab2a300dfef5a177dbb5787b9157
Eric Steele <ems174@psu.edu>
2011-06-24 16:48:22 +0100

    Implement breadcrumb path.

--- src/plone/app/cmsui/addmenu.py
+++ src/plone/app/cmsui/addmenu.py
@@ -1,6 +1,7 @@
 from zope.app.publisher.interfaces.browser import IBrowserMenu
-from zope.component import getUtility
+from zope.component import getMultiAdapter, getUtility
 from zope.publisher.browser import BrowserView
+
 
 class AddMenu(BrowserView):
     """Add menu overlay
@@ -13,5 +14,8 @@ class AddMenu(BrowserView):
         factoriesMenu = getUtility(IBrowserMenu, name='plone_contentmenu_factory', context=self.context)
         self.addable_types = factoriesMenu.getMenuItems(self.context, self.request)
 
-
+        breadcrumbs_view = getMultiAdapter((self.context, self.request),
+                                           name='breadcrumbs_view')
+        self.breadcrumbs = breadcrumbs_view.breadcrumbs()
+        
         return self.index()
