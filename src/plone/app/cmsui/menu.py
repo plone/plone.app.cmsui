6a6a5ad61c519fdef205c50eed67491337621534
Martin Aspeli <optilude@gmail.com>
2011-06-25 11:10:32 +0100

    Make the basic elements work

--- src/plone/app/cmsui/menu.py
+++ src/plone/app/cmsui/menu.py
@@ -2,8 +2,10 @@ from zope.component import getMultiAdapter
 from zope.publisher.browser import BrowserView
 from plone.memoize.instance import memoize
 
+from Acquisition import aq_base
 from AccessControl import getSecurityManager
 from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.interfaces import IPloneSiteRoot
 
 class Menu(BrowserView):
     """The view containing the overlay menu
@@ -16,6 +18,7 @@ class Menu(BrowserView):
         # Commonly useful variables
         self.contextState = getMultiAdapter((self.context, self.request), name=u'plone_context_state')
         self.portalState = getMultiAdapter((self.context, self.request), name=u'plone_portal_state')
+        self.tools = getMultiAdapter((self.context, self.request), name=u'plone_tools')
         self.securityManager = getSecurityManager()
         self.anonymous = self.portalState.anonymous()
         
@@ -45,7 +48,7 @@ class Menu(BrowserView):
         member = self.portalState.member()
         userid = member.getId()
         
-        membership = getToolByName(self.context, 'portal_membership')
+        membership = self.tools.membership()
         memberInfo = membership.getMemberInfo(userid)
         
         fullname = userid
@@ -58,12 +61,53 @@ class Menu(BrowserView):
     
     @memoize
     def userHomeLinkURL(self):
-        if self.securityManager.checkPermission('Portlets: View dashboard', self.context):
-            return "%s/dashboard" % self.portalState.navigation_root_url()
-        else:
-            return "%s/personalize_form" % self.portalState.navigation_root_url()
+        member = self.portalState.member()
+        userid = member.getId()
+        return "%s/author/%s" % (self.portalState.navigation_root_url(), userid)
     
     @memoize
     def breadcrumbs(self):
         breadcrumbsView = getMultiAdapter((self.context, self.request), name='breadcrumbs_view')
         return breadcrumbsView.breadcrumbs()
+    
+    @memoize
+    def modificationDate(self):
+        if hasattr(aq_base(self.context), 'modified'):
+            modifiedDate = self.context.modified()
+        
+            translationService = getToolByName(self.context, 'translation_service')
+            return translationService.ulocalized_time(modifiedDate,
+                    context=self.context,
+                    domain='plonelocales'
+                )
+        
+        return None
+    
+    @memoize
+    def authorName(self):
+        acl_users, owner = self.context.getOwnerTuple()
+        membership = self.tools.membership()
+        memberInfo = membership.getMemberInfo(owner)
+        return memberInfo.get('fullname', '') or owner
+    
+    @memoize
+    def workflowState(self):
+        state = self.contextState.workflow_state()
+        if state is None:
+            return None
+        
+        workflows = self.tools.workflow().getWorkflowsFor(self.context)
+        if workflows:
+            for w in workflows:
+                if w.states.has_key(state):
+                    return w.states[state].title or state
+        
+        return state
+    
+    @memoize
+    def itemsInFolder(self):
+        folder = self.contextState.folder()
+        if IPloneSiteRoot.providedBy(folder):
+            return None
+        
+        return len(folder)
