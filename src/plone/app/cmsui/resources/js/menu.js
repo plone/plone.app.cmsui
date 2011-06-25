ad034f1c791137b2aa10d2ff15eb695a20834b63
Rob Gietema <rob@fourdigits.nl>
2011-06-25 12:11:36 +0200

    Moved event handlers out of config.

--- src/plone/app/cmsui/resources/js/menu.js
+++ src/plone/app/cmsui/resources/js/menu.js
@@ -3,6 +3,18 @@
 /* Code that runs inside the iframe menu
  */
 
+function expandMenu() {
+    offset = $(window.parent).scrollTop();
+    $('body', window.parent.document).css('overflow', 'hidden');
+    $(window.parent).scrollTop(offset);
+    $('#plone-cmsui-menu', window.parent.document).css('height', '100%');
+}
+ function contractMenu() {
+    $('body', window.parent.document).css('overflow', 'auto');
+    $(window.parent).scrollTop(offset);
+    $('#plone-cmsui-menu', window.parent.document).css('height', '139px');
+}
+ 
 (function ($) {
     $().ready(function () {
         var iframe = $('#plone-cmsui-menu', window.parent.document);
@@ -13,16 +25,11 @@
             config: { 
                 top: 150,
                 onBeforeLoad: function (e) { 
-                    offset = $(window.parent).scrollTop();
-                    $('body', window.parent.document).css('overflow', 'hidden');
-                    $(window.parent).scrollTop(offset);
-                    $('#plone-cmsui-menu', window.parent.document).css('height', '100%');
+                    expandMenu();
                     return true; 
                 },
-                 onClose: function (e) { 
-                    $('body', window.parent.document).css('overflow', 'auto');
-                    $(window.parent).scrollTop(offset);
-                    $('#plone-cmsui-menu', window.parent.document).css('height', '139px');
+                onClose: function (e) { 
+                    contractMenu();
                     return true; 
                 } 
             } 
