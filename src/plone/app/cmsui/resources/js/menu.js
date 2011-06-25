6c9b7fd17cca7e1982d00145e303cfb175bf990e
David Glick <dglick@gmail.com>
2011-06-26 00:21:32 +0100

    move structure dialog js to separate file

--- src/plone/app/cmsui/resources/js/menu.js
+++ src/plone/app/cmsui/resources/js/menu.js
@@ -61,6 +61,7 @@ function eraseCookie(name) {
             // Add this to a link or button to make it close the overlay e.g.
             // on cancel without reloading the page
             closeselector: '.overlayCloseAction',
+            formselector: 'form.overlayForm',
             config: { 
                 top: 150,
                 onBeforeLoad: function (e) { 
@@ -77,50 +78,6 @@ function eraseCookie(name) {
                     return true; 
                 }
             } 
-        });
-
-
-        // ------ Structure dialog navigation -------
-        
-        var overlay_location = $('#structure a').attr('href');
-        window.parent.history.replaceState({structure_href: overlay_location}, null, window.parent.location.href);
-
-        var slideTo = function(href, dir) {
-            var $slider = $('.structure-slider');
-            var width = $slider.outerWidth();
-
-            $('#structure-dialog').css({height: $slider.outerHeight()});
-            $('<div class="structure-slider" style="width: 100%"><' + '/div>')
-                .prependTo($('#structure-dialog'))
-                .load(href + ' .structure-slider>*')
-                .css({position: 'absolute', left: (dir=='left'?width:-width), top: 0})
-                .animate({'left': 0}, 200, function() {$('.structure-slider').css('position', 'static'); $('#structure-dialog').css('height', 'auto');});
-	    $slider.css('position', 'relative').animate({'left': (dir=='left')?-width:width}, 200, null, function(){
-		$slider.remove();
-		$("#listing-table").ploneDnD();
-	    });
-            overlay_location = href;
-        }
-
-        $('#structure-dialog a.link-child').live('click', function(e) {
-            e.preventDefault();
-            var href = $(this).attr('href');
-            slideTo(href, 'left');
-            window.parent.history.pushState({structure_href: href}, null, window.parent.location.href);
-        });
-
-        $('#structure-dialog a.link-parent').live('click', function(e) {
-            e.preventDefault();
-            var href = $(this).attr('href');
-            slideTo(href, 'right');
-            window.parent.history.pushState({structure_href: href}, null, window.parent.location.href);
-        });
-
-        window.parent.addEventListener('popstate', function(e) {
-            if (e.state != null && e.state.structure_href !== undefined) {
-                var href = e.state.structure_href;
-                slideTo(href, (overlay_location.length > href.length ? 'right' : 'left'));
-            }
         });
 
     });
