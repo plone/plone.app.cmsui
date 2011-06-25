f6104742b03a5efcfd020f505cf7a28405b8349a
Eric Steele <ems174@psu.edu>
2011-06-25 19:03:47 +0100

    Set value of the type_name field, hide it.

--- src/plone/app/cmsui/addmenu.py
+++ src/plone/app/cmsui/addmenu.py
@@ -10,7 +10,7 @@ from zope.interface import implements
 from zope.publisher.browser import BrowserView
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
+from z3c.form.interfaces import HIDDEN_MODE
 
 class AddableTypesVocabulary(object):
     implements(IVocabularyFactory)
@@ -32,7 +32,7 @@ AddableTypesVocabularyFactory = AddableTypesVocabulary()
 class IAddNewContent(interface.Interface):
 
     title = schema.TextLine(title=u"Title")
-    content_type = schema.TextLine(title=u"Type")
+    type_name = schema.TextLine(title=u"Type")
 
 
 class AddNewContentForm(form.Form):
@@ -41,6 +41,12 @@ class AddNewContentForm(form.Form):
     ignoreContext = True # don't use context to get widget data
     label = "Add content"
     
+    def update(self):
+        tn = self.fields['type_name']
+        tn.mode = HIDDEN_MODE
+        tn.field.default = unicode(getattr(self.request, 'type_name', ''))
+        super(AddNewContentForm, self).update()
+
     @button.buttonAndHandler(u'Add content')
     def handleApply(self, action):
         data, errors = self.extractData()
@@ -62,8 +68,9 @@ class AddNewContentForm(form.Form):
         id = chooser._findUniqueName(id, None)
 
         # create the object
-        container.invokeFactory(data['content_type'], id=id, title=title)
+        container.invokeFactory(data['type_name'], id=id, title=title)
         # redirect to immediate_view
+        
         self.request.response.redirect("%s/edit" % container[id].absolute_url())
         # open edit overlay    
 
