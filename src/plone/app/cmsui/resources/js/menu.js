9a6d46b2456adb519d2cbab39bb555f1d744c3ee
Rob Gietema <rob@fourdigits.nl>
2011-06-25 11:45:24 +0200

    Added onbeforeload an onclose handlers to the overlay.

--- src/plone/app/cmsui/resources/js/menu.js
+++ src/plone/app/cmsui/resources/js/menu.js
@@ -6,14 +6,30 @@
 (function ($) {
     $().ready(function () {
         var iframe = $('#plone-cmsui-menu', window.parent.document);
+        var offset;
         $('a.overlayLink').prepOverlay({
             subtype: 'ajax',
-            filter: common_content_filter
-            });
+            filter: common_content_filter,
+            config: { 
+                top: 150,
+                onBeforeLoad: function (e) { 
+                    offset = $(window.parent).scrollTop();
+                    $('body', window.parent.document).css('overflow', 'hidden');
+                    $(window.parent).scrollTop(offset);
+                    $('#plone-cmsui-menu', window.parent.document).css('height', '100%');
+                    return true; 
+                },
+                 onClose: function (e) { 
+                    $('body', window.parent.document).css('overflow', 'auto');
+                    $(window.parent).scrollTop(offset);
+                    $('#plone-cmsui-menu', window.parent.document).css('height', '139px');
+                    return true; 
+                } 
+            } 
+        });
 
-        iframe.height('100%');
-        $(window.parent.document.body).css('overflow', 'hidden');
-        //iframe.height($('#visual-portal-wrapper').height());
+        // iframe.height($('#visual-portal-wrapper').outerHeight());
+        iframe.height(139);
 
     });
 }(jQuery));
