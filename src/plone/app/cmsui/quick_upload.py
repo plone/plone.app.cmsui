0543846b032123bf24a0cb69b035a4719e1bede0
Eric Steele <ems174@psu.edu>
2011-06-26 18:28:31 +0100

    No need for Plone 3 support.

--- src/plone/app/cmsui/quick_upload.py
+++ src/plone/app/cmsui/quick_upload.py
@@ -93,14 +93,7 @@ def _listTypesForInterface(context, interface):
     @return: ['Image', 'News Item']
     """
     archetype_tool = getToolByName(context, 'archetype_tool')
-    #plone4
-    try :
-        all_types = [tipe.getId() for tipe in archetype_tool.listPortalTypesWithInterfaces([interface])]
-    #plone3
-    except :
-        all_types = archetype_tool.listRegisteredTypes(inProject=True)
-        all_types = [tipe['portal_type'] for tipe in all_types
-                     if interface.isImplementedByInstancesOf(tipe['klass'])]
+    all_types = [tipe.getId() for tipe in archetype_tool.listPortalTypesWithInterfaces([interface])]
     return dict.fromkeys(all_types).keys() 
 
 class QuickUploadView(BrowserView):
