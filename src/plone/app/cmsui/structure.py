bfcdce18e7a2192c919cb9efc12f92dec9ec6c0a
Nathan Van Gheem <vangheem@gmail.com>
2011-06-28 09:37:24 -0500

    rename everything to be more Zope/Plone like since menu.py is naming this way...

--- src/plone/app/cmsui/structure.py
+++ src/plone/app/cmsui/structure.py
@@ -33,26 +33,24 @@ class StructureView(BrowserView):
         registry = getUtility(IRegistry)
         settings = registry.forInterface(ICMSUISettings, False)
         
-        
         self.pagesize = settings.folderContentsBatchSize
-        self.show_all = self.request.get('show_all', '').lower() == 'true'
+        self.showAll = self.request.get('show_all', '').lower() == 'true'
 
-        selection = self.request.get('select')
-        if selection == 'all':
-            self.selectall = True
-        else:
-            self.selectall = False
+        selection = self.request.get('select', '').lower() == 'all'
+        return self.index()
 
-        self.context_state = getMultiAdapter((self.context, self.request), 
+
+    @lazy_property
+    def contextState(self):
+        return getMultiAdapter((self.context, self.request), 
             name=u'plone_context_state')
 
-        return self.index()
 
     def breadcrumbs(self):
         breadcrumbsView = getMultiAdapter((self.context, self.request), 
             name='breadcrumbs_view')
         breadcrumbs = list(breadcrumbsView.breadcrumbs())
-        if self.context_state.is_default_page():
+        if self.contextState.is_default_page():
             # then we need to mess with the breadcrumbs a bit.
             parent = aq_parent(aq_inner(self.context))
             if breadcrumbs:
@@ -66,6 +64,7 @@ class StructureView(BrowserView):
                 )
         return breadcrumbs
 
+
     def contentsMethod(self):
         context = aq_inner(self.context)
         if IATTopic.providedBy(context):
@@ -74,19 +73,20 @@ class StructureView(BrowserView):
             contentsMethod = context.getFolderContents
         return contentsMethod
 
+
     @lazy_property
-    def folderitems(self):
+    def folderItems(self):
         """
         """
         context = aq_inner(self.context)
-        plone_utils = getToolByName(context, 'plone_utils')
-        plone_view = getMultiAdapter((context, self.request), name=u'plone')
-        plone_layout = getMultiAdapter((context, self.request), name=u'plone_layout')
-        portal_workflow = getToolByName(context, 'portal_workflow')
-        portal_types = getToolByName(context, 'portal_types')
-        portal_membership = getToolByName(context, 'portal_membership')
+        ploneUtils = getToolByName(context, 'plone_utils')
+        ploneView = getMultiAdapter((context, self.request), name=u'plone')
+        ploneLayout = getMultiAdapter((context, self.request), name=u'plone_layout')
+        portalWorkflow = getToolByName(context, 'portal_workflow')
+        portalTypes = getToolByName(context, 'portal_types')
+        portalMembership = getToolByName(context, 'portal_membership')
 
-        browser_default = plone_utils.browserDefault(context)
+        browser_default = ploneUtils.browserDefault(context)
 
         contentsMethod = self.contentsMethod()
 
@@ -99,70 +99,59 @@ class StructureView(BrowserView):
 
             # avoid creating unnecessary info for items outside the current
             # batch;  only the path is needed for the "select all" case...
-            if not self.selectall and not self.show_all and not start <= i < end:
+            if not self.selectAll and not self.showAll and not start <= i < end:
                 results.append(dict(path = path))
                 continue
 
             if (i + 1) % 2 == 0:
-                table_row_class = "draggable even"
+                tableRowClass = "draggable even"
             else:
-                table_row_class = "draggable odd"
+                tableRowClass = "draggable odd"
 
             url = obj.getURL()
-            icon = plone_layout.getIcon(obj)
-            type_class = 'contenttype-' + plone_utils.normalizeString(
-                obj.portal_type)
-
-            review_state = obj.review_state
-            state_class = 'state-' + plone_utils.normalizeString(review_state)
-            relative_url = obj.getURL(relative=True)
-
-            fti = portal_types.get(obj.portal_type)
+            viewUrl = url + "/cmsui-structure"
+            
+            fti = portalTypes.get(obj.portal_type)
             if fti is not None:
-                type_title_msgid = fti.Title()
+                typeTitleMsgid = fti.Title()
             else:
-                type_title_msgid = obj.portal_type
-            url_href_title = u'%s: %s' % (translate(type_title_msgid,
-                                                    context=self.request),
-                                          safe_unicode(obj.Description))
-
+                typeTitleMsgid = obj.portal_type
+            urlHrefTitle = u'%s: %s' % (translate(typeTitleMsgid,
+                                            context=self.request),
+                                            safe_unicode(obj.Description))
             creator = obj.Creator
-            memberInfo = portal_membership.getMemberInfo(creator)
+            memberInfo = portalMembership.getMemberInfo(creator)
             creator = memberInfo.get('fullname', '') or creator
 
-            modified = plone_view.toLocalizedTime(
+            modified = ploneView.toLocalizedTime(
                 obj.ModificationDate, long_format=1)
-
-            obj_type = obj.Type
-            view_url = url + "/cmsui-structure"
-
-            is_browser_default = len(browser_default[1]) == 1 and (
+            
+            isBrowserDefault = len(browser_default[1]) == 1 and (
                 obj.id == browser_default[1][0])
 
             results.append(dict(
                 url = url,
-                url_href_title = url_href_title,
+                urlHrefTitle = urlHrefTitle,
                 id = obj.getId,
-                quoted_id = urllib.quote_plus(obj.getId),
+                quotedId = urllib.quote_plus(obj.getId),
                 path = path,
-                title_or_id = safe_unicode(pretty_title_or_id(plone_utils, obj)),
-                obj_type = obj_type,
+                titleOrId = safe_unicode(pretty_title_or_id(ploneUtils, obj)),
                 creator = creator,
                 modified = modified,
-                icon = icon.html_tag(),
-                type_class = type_class,
-                wf_state = review_state,
-                state_title = portal_workflow.getTitleForStateOnType(review_state,
-                                                           obj_type),
-                state_class = state_class,
-                is_browser_default = is_browser_default,
+                icon = ploneLayout.getIcon(obj).html_tag(),
+                typeClass = 'contenttype-' + ploneUtils.normalizeString(obj.portal_type),
+                wf_state =  obj.review_state,
+                stateTitle = portalWorkflow.getTitleForStateOnType(obj.review_state,
+                                                           obj.Type),
+                stateClass = 'state-' + ploneUtils.normalizeString(obj.review_state),
+                isBrowserDefault = isBrowserDefault,
                 folderish = obj.is_folderish,
-                relative_url = relative_url,
-                view_url = view_url,
-                table_row_class = table_row_class,
-                is_expired = isExpired(obj),
+                viewUrl = viewUrl,
+                tableRowClass = tableRowClass,
+                isExpired = isExpired(obj),
             ))
         return results
+
 
     @property
     @instance.memoize
@@ -179,41 +168,45 @@ class StructureView(BrowserView):
         ordering = context.getOrdering()
         return IExplicitOrdering.providedBy(ordering)
 
+
     @property
-    def show_sort_column(self):
+    def showSortColumn(self):
         return self.orderable and self.editable
+
 
     @property
     def editable(self):
         """
         """
-        return self.context_state.is_editable()
+        return self.contextState.is_editable()
+
 
     @property
     def buttons(self):
         buttons = []
         context = aq_inner(self.context)
-        portal_actions = getToolByName(context, 'portal_actions')
-        button_actions = portal_actions.listActionInfos(
+        portalActions = getToolByName(context, 'portal_actions')
+        buttonActions = portalActions.listActionInfos(
             object=aq_inner(self.context), categories=('folder_buttons',))
 
         # Do not show buttons if there is no data, unless there is data to be
         # pasted
-        if not len(self.folderitems):
+        if not len(self.folderItems):
             if self.context.cb_dataValid():
-                for button in button_actions:
+                for button in buttonActions:
                     if button['id'] == 'paste':
-                        return [self.setbuttonclass(button)]
+                        return [self.setButtonClass(button)]
             else:
                 return []
 
-        for button in button_actions:
+        for button in buttonActions:
             # Make proper classes for our buttons
             if button['id'] != 'paste' or context.cb_dataValid():
-                buttons.append(self.setbuttonclass(button))
+                buttons.append(self.setButtonClass(button))
         return buttons
 
-    def setbuttonclass(self, button):
+
+    def setButtonClass(self, button):
         if button['id'] == 'paste':
             button['cssclass'] = 'standalone'
         else:
@@ -221,82 +214,91 @@ class StructureView(BrowserView):
         return button
 
 
-    def msg_select_item(self, item):
-        title_or_id = (item.get('title_or_id') or item.get('title') or
+    def msgSelectItem(self, item):
+        titleOrId = (item.get('titleOrId') or item.get('title') or
                        item.get('Title') or item.get('id') or item['getId'])
         return _(u'checkbox_select_item',
                  default=u"Select ${title}",
-                 mapping={'title': safe_unicode(title_or_id)})
+                 mapping={'title': safe_unicode(titleOrId)})
 
-    def set_checked(self, item):
-        item['checked'] = self.selectall and 'checked' or None
-        item['table_row_class'] = item.get('table_row_class', '')
 
-    @property
-    @instance.memoize
+    def setChecked(self, item):
+        item['checked'] = self.selectAll and 'checked' or None
+        item['tableRowClass'] = item.get('tableRowClass', '')
+
+
+    @lazy_property
     def batch(self):
         pagesize = self.pagesize
-        if self.show_all:
-            pagesize = len(self.folderitems)
-        b = Batch(self.folderitems,
+        if self.showAll:
+            pagesize = len(self.folderItems)
+        b = Batch(self.folderItems,
                   pagesize=pagesize,
                   pagenumber=1)
-        map(self.set_checked, b)
+        map(self.setChecked, b)
         return b
+
 
     # options
     _select_all = False
-    def _get_select_all(self):
+    def _getSelectAll(self):
         return self._select_all
 
-    def _set_select_all(self, value):
+    def _setSelectAll(self, value):
         self._select_all = bool(value)
+    selectAll = property(_getSelectAll, _setSelectAll)
 
-    selectall = property(_get_select_all, _set_select_all)
 
     @lazy_property
-    def view_url(self):
+    def viewUrl(self):
         return self.context.absolute_url() + '/cmsui-structure'
 
-    @property
-    def selectall_url(self):
-        return self.selectnone_url+'&select=all'
 
     @property
-    def selectnone_url(self):
-        base = self.view_url
-        if self.show_all:
+    def selectallUrl(self):
+        return self.selectnoneUrl+'&select=all'
+
+
+    @property
+    def selectnoneUrl(self):
+        base = self.viewUrl
+        if self.showAll:
             base += '&show_all=true'
         return base
 
+
     @property
-    def show_all_url(self):
-        base = self.view_url + '?show_all=true'
-        if self.selectall:
+    def showAllUrl(self):
+        base = self.viewUrl + '?show_all=true'
+        if self.selectAll:
             base += '&select=all'
         return base
 
-    def quote_plus(self, string):
+
+    def quotePlus(self, string):
         return urllib.quote_plus(string)
 
 
     @instance.memoize
-    def object_buttons(self):
-        """Get the personal actions
+    def objectButtons(self):
         """
-        
+        Get the Actions for the current item.
+        """
         actions = []
-        for action in self.context_state.actions('object_buttons'):
+        for action in self.contextState.actions('object_buttons'):
             actions.append({
                 'id': action['id'],
                 'url': action['url'],
                 'title': action['title'],
                 'description': action['description'],
-            })
-        
+            })        
         return actions
 
-    def object_info(self):
+
+    def objectInfo(self):
+        """
+        Retrieve information on the current item.
+        """
         info = []
         pt = getToolByName(self.context, 'portal_types')
         fti = pt.listTypeTitles().get(self.context.portal_type, 'unknown')
@@ -338,6 +340,6 @@ class MoveItem(BrowserView):
             self.context.REQUEST.response.setStatus('BadRequest')
             return str(e)
 
-        plone_utils = getToolByName(self.context, 'plone_utils')
-        plone_utils.reindexOnReorder(self.context)
+        ploneUtils = getToolByName(self.context, 'plone_utils')
+        ploneUtils.reindexOnReorder(self.context)
         return "<done />"
